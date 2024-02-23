6335d122ee61eae3ddc8a47a3abde81d01d3574e
xcode73 <xcode.uln@gmail.com>
2024-02-23 16:10:39 +0400

    swift-tools-version:5.9

--- Package.swift
+++ Package.swift
@@ -1,30 +1,29 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "feather-icons",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v10_15)
     ],
     products: [
-        .library(name: "FeatherIcons", targets: ["FeatherIcons"]),
+        .library(name: "FeatherIcons", targets: ["FeatherIcons"])
     ],
     dependencies: [
         .package(url: "https://github.com/binarybirds/swift-html", from: "1.7.0"),
-        .package(url: "https://github.com/cezheng/Fuzi", from: "3.1.3"),
+        .package(url: "https://github.com/cezheng/Fuzi", from: "3.1.3")
     ],
     targets: [
         .executableTarget(name: "Converter", dependencies: [
-            .product(name: "Fuzi", package: "Fuzi"),
+            .product(name: "Fuzi", package: "Fuzi")
         ]),
-        
         .target(name: "FeatherIcons", dependencies: [
-            .product(name: "SwiftSvg", package: "swift-html"),
+            .product(name: "SwiftSvg", package: "swift-html")
         ]),
-
         .testTarget(name: "FeatherIconsTests", dependencies: [
-            .target(name: "FeatherIcons"),
+            .target(name: "FeatherIcons")
         ]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
 
